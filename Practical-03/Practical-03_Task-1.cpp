14017014dabe36c8bfabd4291a86721356a174ac
KhushiSingh006 <91174547+KhushiSingh006@users.noreply.github.com>
2021-10-02 23:11:35 +0530

    Add files via upload

---
+++ Practical-03/Practical-03_Task-1.cpp
@@ -0,0 +1,8 @@
+#include<iostream.h>
+using name sapce std;
+
+{
+cout<<hello world;
+
+}
+
---
+++ Practical-03/Practical-03_Task-3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+char a;
+cout<<"Enter the value: "<<endl;
+cin>>a;
+cout<<"Value is : "<<a<<endl;
+return 0;
+
+}
