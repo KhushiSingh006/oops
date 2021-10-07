14a25cbb58f03a7f82f1927d5d0abcc7e9eae8db
KhushiSingh006 <91174547+KhushiSingh006@users.noreply.github.com>
2021-10-07 23:00:09 +0530

    Add files via upload

---
+++ Practical-06/Practical-06_Task-1.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a=4;
+string s="Khushi";
+cout<<"Adding integer and a string:"<<s+a<<endl;
+return 0;
+}
---
+++ Practical-06/Practical-06_Task-4.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string s1,s2;
+    cout<<"Enter the first string:";
+    cin>>s1;
+    cout<<"Enter the second string:";
+    cin>>s2;
+
+    if(s1==s2)
+       cout<<"Strings are EQUAL";
+    else
+       cout<<"Strings are NOT EQUAL";
+    
+    
+    
+    return 0;
+}
